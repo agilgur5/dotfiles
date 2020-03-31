e4e0205039199b66f8c3ef82d0b30ec2243d6924
Anton Gilgur <agilgur5@gmail.com>
2020-03-31 18:19:46 -0400

    update generated configs
    - remove standard from various js configs
      - extensions isn't well updated and better to have per-project
        config (esp. since multiple that I work on have different lint
        settings), so just use ESLint and eslint-config-standard locally
    
    - add git prompt for a nicer and very helpful PS1 bash prompt
    - add nvm and pyenv for handling different versions of Node + Python
    - add yarn for faster and cleaner Node package management
    - add helpful GitLens VSCode extension
    
    - update VSCode settings
      - make sure all open tabs are saved on close + re-open
      - add a bunch of files to search ignore list
        - mostly around code coverage and different package managers
      - enable breadcrumbs in the editor for quick at-a-glance etc
      - register Google App Script as JS for syntax highlighting
        - not quite the same thing, but close enough

--- VSCode/extensions.sh
+++ VSCode/extensions.sh
@@ -1,4 +1,6 @@
-code --install-extension chenxsan.vscode-standardjs
 code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension drKnoxy.eslint-disable-snippets
+code --install-extension eamodio.gitlens
 code --install-extension ms-python.python
 code --install-extension samverschueren.final-newline
