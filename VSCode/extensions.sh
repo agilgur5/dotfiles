690fab61d714cd1a11b5a1fadc6fa51a46c05613
Anton Gilgur <agilgur5@gmail.com>
2020-11-15 19:34:55 -0500

    feat: add various k8s tools
    - I never quite saved work configs, but doing so now as it's helpful for
      the future when doing other k8s work instead of starting up a k8s
      dev env from scratch
    
    - add kube_ps1 for a bash k8s prompt
      - git prompt would overwrite this, so use the env var/function
        `prompt_callback` to add `kube_ps1` to the git prompt instead
      - and remove GIT_PROMPT_ONLY_IN_REPO so that we can get the k8s prompt
        without being in a git repo
      - also add several comments
    
    - add a VSCode plugin for YAML
    
    - add Homebrew packages for commonly used helpers like k9s,
      kube-capacity, spruce, and jq
    - add Homebrew packages for common k8s tools like Helm, k8s cli,
      and kops
      - at NS8, several of these were locally installed, then used in
        Concourse Resources
        - would be good to have all of these in a common Dockerfile/image
          for CI and standardized dev env use

--- VSCode/extensions.sh
+++ VSCode/extensions.sh
@@ -3,4 +3,5 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension drKnoxy.eslint-disable-snippets
 code --install-extension eamodio.gitlens
 code --install-extension ms-python.python
+code --install-extension redhat.vscode-yaml
 code --install-extension samverschueren.final-newline
