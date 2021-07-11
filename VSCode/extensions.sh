df5346c4765267eb7ffd00848bb6d7d2052e9034
Anton Gilgur <agilgur5@gmail.com>
2021-07-11 01:18:35 -0400

    feat: use latest Bash, add shellcheck to VSCode, add grep, perl
    - upgrade default macOS Bash to Bash v5
      - default in macOS is Bash v3 which is old and causes lots of issues
    - use the upgraded Homebrew installed Bash by default in VSCode
    
    - add bash-debug extension for VSCode to get shellcheck in VSCode
      - pretty sure I've installed this before but never added it to my
        dotfiles
      - and add some shellcheck settings
    
    - also add GNU grep and perl as helpful shell utilities

--- VSCode/extensions.sh
+++ VSCode/extensions.sh
@@ -5,5 +5,6 @@ code --install-extension drKnoxy.eslint-disable-snippets
 code --install-extension eamodio.gitlens
 code --install-extension ms-python.python
 code --install-extension redhat.vscode-yaml
+code --install-extension rogalmic.bash-debug
 code --install-extension samverschueren.final-newline
 code --install-extension timonwong.shellcheck
