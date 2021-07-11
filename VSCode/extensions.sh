b0c74d198c25e94b1ec7efaf87b6bb67092fc52d
Anton Gilgur <agilgur5@gmail.com>
2021-07-11 01:33:00 -0400

    feat: add OPA / Rego tooling
    - add opa CLI to Brew
    - add regolint extension to VSCode
    - add opa extension to VSCode
    
    - haven't added conftest or regula etc yet here, should get to those too

--- VSCode/extensions.sh
+++ VSCode/extensions.sh
@@ -4,7 +4,9 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension drKnoxy.eslint-disable-snippets
 code --install-extension eamodio.gitlens
 code --install-extension ms-python.python
+code --install-extension plex.vscode-regolint
 code --install-extension redhat.vscode-yaml
 code --install-extension rogalmic.bash-debug
 code --install-extension samverschueren.final-newline
 code --install-extension timonwong.shellcheck
+code --install-extension tsandall.opa
