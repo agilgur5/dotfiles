6291cc3ff65f8b33361c7af050f5422ce628d633
Anton Gilgur <agilgur5@gmail.com>
2022-08-17 13:57:12 -0400

    feat(VSCode): add Go, Bats, and Svelte plugins
    - couple of things I've added in the past year (ish?), mostly for syntax highlighting

--- VSCode/extensions.sh
+++ VSCode/extensions.sh
@@ -4,11 +4,14 @@ code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension drKnoxy.eslint-disable-snippets
 code --install-extension eamodio.gitlens
+code --install-extension golang.go
+code --install-extension jetmartin.bats
 code --install-extension ms-python.python
 code --install-extension ms-toolsai.jupyter
 code --install-extension plex.vscode-regolint
 code --install-extension redhat.vscode-yaml
 code --install-extension rogalmic.bash-debug
 code --install-extension samverschueren.final-newline
+code --install-extension svelte.svelte-vscode
 code --install-extension timonwong.shellcheck
 code --install-extension tsandall.opa
