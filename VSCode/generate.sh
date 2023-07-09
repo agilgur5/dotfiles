2e46e2fa6375eff2a10b05fc7ab90ea5b9a5cfb0
Anton Gilgur <agilgur5@gmail.com>
2023-07-09 11:44:58 -0400

    fix(VSCode): add shebang to top of `extensions.sh` first
    - then add all the extension install commands after
    
    - without this, shebang would be accidentally deleted and I had to workaround that in `git`

--- VSCode/generate.sh
+++ VSCode/generate.sh
@@ -3,4 +3,5 @@ echo 'Generating settings.json...';
 cat ~/Library/Application\ Support/Code/User/settings.json > settings.json;
 
 echo 'Generating extensions.sh...';
-code --list-extensions | xargs -L 1 echo code --install-extension > extensions.sh;
+echo "#!/bin/bash" > extensions.sh;
+code --list-extensions | xargs -L 1 echo code --install-extension >> extensions.sh;
